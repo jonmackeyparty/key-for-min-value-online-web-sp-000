15ca1b61b28d4119985e5676ce16dedd47601730
Jon Mackey <jon@jonmackey.party>
2018-11-26 22:21:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,15 @@
 def key_for_min_value(name_hash)
   min_val = 1000
   min_key = 0
-  if name_hash == {}
-    nil
-  else name_hash.each do |key, value| 
+  name_hash.each do |key, value| 
     if value < min_val 
          min_val = value
          min_key = key
     end
   end
-end
-  min_key
+  if name_hash == {}
+    nil 
+  else
+    min_key
+  end
 end
